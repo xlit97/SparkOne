05133a786d5dd60cd1adb7596bd24e75eeae5b89
xlit97 <xlit97@yandex.ru>
2022-04-15 12:11:02 +0600

    Сервер. Добавил обработку POST /location который просто выводит лог location

--- ktor-weather-server/src/main/java/com/maxwellscore/ktor_weather_server/Server.kt
+++ ktor-weather-server/src/main/java/com/maxwellscore/ktor_weather_server/Server.kt
@@ -2,12 +2,16 @@ package com.maxwellscore.ktor_weather_server
 
 import io.ktor.application.*
 import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.serialization.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import org.slf4j.LoggerFactory
 import kotlin.random.Random
+
 
 private val cities = listOf("Якутск", "Москва", "Алматы")
 private val types = listOf("cloudy_weather", "sunny_weather", "rain_is_pouring")
@@ -28,6 +32,11 @@ fun main() {
                     weatherType = types.random()
                 )
                 call.respond(weatherData)
+            }
+            post("/location") {
+                val posted = call.receiveText()
+                call.respondText("Местоположение получено", status = HttpStatusCode.Accepted)
+                LoggerFactory.getLogger("MyLogger").info("Тек. местоположение: $posted")
             }
         }
     }.start(wait = true)
